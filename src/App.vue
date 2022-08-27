dff27bea3bec30abca319aaec100a97fe6aa240f
ZAP <zhangaiping11011@163.com>
2022-08-27 15:03:57 +0800

    'vant组件库配置完成'

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <div id="app">
     <router-view/>
     <h1>移动端</h1>
+    <div>
+      <i class="iconfont icon-RectangleCopy11"></i>
+    </div>
+
+    <div>-----------------------------------------</div>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
@@ -12,5 +23,11 @@ export default {
 </script>
 
 <style lang="less">
-
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
 // 加载全局样式
 import '@/styles/index.less'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
